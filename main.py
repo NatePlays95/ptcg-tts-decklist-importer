beb84a4a65582fdc8be6e029bbf9faba9b2a7a1b
Thomaz <thomaz2pinheiro@gmail.com>
2024-02-08 02:02:05 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 from modules.parser import parseCardFromLine
 
+# SDK
+from pokemontcgsdk import Card, RestClient
+
+# CHAVE DA API (fora do repositorio)
+from api_key import api_key
 
 input = "1 Professor's Letter BKT 146a"
 cardInfo = parseCardFromLine(input)
-print(cardInfo)
+
+key = api_key()
+RestClient.configure(key)
+
+nome_arquivo = 'test.txt'
+linhas = []
+try:
+    with open(nome_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()
+except FileNotFoundError:
+    print(f'O arquivo {nome_arquivo} não foi encontrado.')
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
+
+for linha in linhas:
+    cardInfo = parseCardFromLine(linha.strip())
+    setName = cardInfo['setName']
+    setNumber = cardInfo['setNumber']
+    card = Card.where(q=f'set.ptcgoCode:{setName} number:{setNumber}')
+    if (card != []):
+        print(card[0].name)
+    else:  
+        print('nada')
+
+
+
+cards = Card.where(q='set.ptcgoCode:CRE number:22')
+print(cards)
+
